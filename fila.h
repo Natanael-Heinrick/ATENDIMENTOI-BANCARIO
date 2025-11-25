2ef8916c5804ad4198cce8f1da669d8f4b241b10
Natanael-Heinrick <natanaelheinrick12@gmail.com>
2025-11-25 20:48:22 -0300

    Refactor fila.h para melhorar a estrutura e as funções da fila
    Estrutura Fila atualizada para incluir 'quantidade' e modificada a definição de 'Fila'. Incluído 'cliente.h' e adicionadas novas funções para verificar se a fila está cheia.
    
    Signed-off-by: Natanael-Heinrick <natanaelheinrick12@gmail.com>

--- fila.h
+++ fila.h
@@ -1,45 +1,22 @@
 #ifndef FILA_H
 #define FILA_H
-#include "error.h"
 
-#define MAX_FILA 100
+#include "cliente.h"
 
+#define MAX_FILA 100  // Corrigido: Movido para o .h
 
-
-typedef struct cliente
-{
-  char nome[50];
-  int idade;
-  int prioridade;
-} Cliente;
-typedef struct fila
-{
+typedef struct {
     Cliente clientes[MAX_FILA];
     int inicio;
     int fim;
-  } Fila;
-
-
-
-
-typedef struct filaPrioritaria {
-    Cliente *array;
-    int tamanho;
-    int capacidade;
-} FilaPrioritaria;
-
-FilaPrioritaria* criarFilaPrioritaria(int capacidade);
-
+    int quantidade;
+} Fila;
 
 void inicializarFila(Fila *f);
-int  filaVazia(Fila *f);
-int  enfileirar(Fila *f, Cliente c);
+int filaVazia(Fila *f);
+int filaCheia(Fila *f);
+int enfileirar(Fila *f, Cliente c);
 Cliente desenfileirar(Fila *f);
 void exibirFila(Fila *f);
-
-void inserirClientePrioritario(FilaPrioritaria *f, Cliente cliente); // O(log n)
-Cliente removerClientePrioritario(FilaPrioritaria *f); // O(log n)
-int filaPrioritariaVazia(FilaPrioritaria *f);
-void exibirFilaPrioritaria(FilaPrioritaria *f);
 
 #endif
